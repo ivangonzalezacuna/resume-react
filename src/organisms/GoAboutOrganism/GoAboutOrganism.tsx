8f40192191ee4c6e92bae29c905d6e160deb9030
Iván González Acuña <ivan.acuna@fitted.group>
2024-08-20 21:40:09 +0200

    Tiny simplication in GoAbout

--- src/organisms/GoAboutOrganism/GoAboutOrganism.tsx
+++ src/organisms/GoAboutOrganism/GoAboutOrganism.tsx
@@ -6,13 +6,11 @@ export const GoAboutOrganism = () => {
   const [t] = useTranslation("home");
 
   return (
-    <>
-      <Container to={t("aboutHref")}>
-        <Wrapper whileHover="hover" variants={container}>
-          <Text>{t("aboutButton")}</Text>
-          <Icon />
-        </Wrapper>
-      </Container>
-    </>
+    <Container to={t("aboutHref")}>
+      <Wrapper whileHover="hover" variants={container}>
+        <Text>{t("aboutButton")}</Text>
+        <Icon />
+      </Wrapper>
+    </Container>
   );
 };
