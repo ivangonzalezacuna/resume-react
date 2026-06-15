5b51e85c5e2308b3cdb87208ef22334f4d268a60
Iván González Acuña <ivan.acuna@fitted.group>
2026-06-15 08:47:28 +0200

    perf(fonts): self-host via fontsource, remove Google Fonts requests

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,8 @@
+import "@fontsource/plus-jakarta-sans/400.css";
+import "@fontsource/plus-jakarta-sans/500.css";
+import "@fontsource/plus-jakarta-sans/700.css";
+import "@fontsource/jetbrains-mono/400.css";
+import "@fontsource/jetbrains-mono/500.css";
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
